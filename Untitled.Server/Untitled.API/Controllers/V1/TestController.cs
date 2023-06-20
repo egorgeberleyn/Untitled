8f78c1428b73cdc44eaaf8901dfe72a6aad6b4f9
Egor <egeberleyn@gmail.com>
2023-06-20 12:19:02 +0300

    Add api versioning with swagger integration

---
+++ Untitled.Server/Untitled.API/Controllers/V1/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Untitled.API.Controllers.V1;
+
+public class TestController : ApiController
+{
+    [HttpGet]
+    [Route("test")]
+    [ApiVersion("1.0")]
+    [ApiVersion("2.0")]
+    public IActionResult Get()
+    {
+        return Ok("That's good");
+    }
+}
