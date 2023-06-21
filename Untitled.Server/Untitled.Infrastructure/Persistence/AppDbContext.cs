fe22e0f1ae8526d851238bb60e70355d017fb381
Egor <egeberleyn@gmail.com>
2023-06-21 14:12:34 +0300

    Add dbcontext

---
+++ Untitled.Server/Untitled.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Untitled.Infrastructure.Persistence;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options): base(options) { }
+}
